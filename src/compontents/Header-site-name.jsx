1c33d10db3de6d004232f66e38d6221910d78744
kamik0t0 <Cap_NEMOx86@inbox.ru>
2022-02-02 22:30:43 +0500

    sitename change

--- src/compontents/Header-site-name.jsx
+++ src/compontents/Header-site-name.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import classes from "./Header-site-name.module.css";
 
 export default function SiteName() {
-    return <div className={classes.header_site_name}>Бизнес ассистент</div>;
+    return <div className={classes.header_site_name}>Бизнес-ассистент</div>;
 }
