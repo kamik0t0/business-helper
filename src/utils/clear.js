a6a6db572fcb224676e08c416129fcd33f8443f0
kamik0t0 <Cap_NEMOx86@inbox.ru>
2022-06-14 00:31:15 +0500

    minor test commit

--- src/utils/clear.js
+++ src/utils/clear.js
@@ -4,4 +4,25 @@ export function clear() {
     }
 }
 
-/* merge */
+function makeCounter() {
+    let counter = 0;
+    // @ts-check
+
+    /**
+     * @return {number}
+     */
+    return function () {
+        return counter++;
+    };
+}
+
+let counter1 = makeCounter();
+let counter2 = makeCounter();
+
+console.log(counter1);
+
+console.log(counter1());
+console.log(counter1());
+console.log(counter1());
+console.log(counter2());
+console.log(counter2());
